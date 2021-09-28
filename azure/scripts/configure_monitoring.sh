c40491f6bf0d6abd5ac4b1d61acafd32910163b7
Shibu Gope <shibug@gmail.com>
2021-09-28 18:33:51 -0500

    changes

--- azure/scripts/configure_monitoring.sh
+++ azure/scripts/configure_monitoring.sh
@@ -1,7 +1,10 @@
+#Create a user-defined network
+docker network create monitoring
+
 #Run Prometheus
-docker run -d -h status.cryptofarmers.io -v prometheus:/prometheus -v /data/monitoring/prometheus.yml:/etc/prometheus/prometheus.yml \
-  -p 9090:9090 --name prometheus --restart on-failure:3 --security-opt="no-new-privileges=true" \
-  --health-cmd='wget -qO- localhost:9090/status || exit 1' --health-start-period=3m \
+docker run -d -p 9090:9090 --name prometheus --restart on-failure:3 --security-opt="no-new-privileges=true" \
+  -v /data/monitoring/prometheus/db:/prometheus -v /data/monitoring/prometheus/config.yml:/etc/prometheus/prometheus.yml \
+  --network monitoring --health-cmd='wget -qO- localhost:9090/status || exit 1' --health-start-period=3m \
   prom/prometheus:v2.30.0 \
   --config.file=/etc/prometheus/prometheus.yml \
   --web.console.libraries=/etc/prometheus/console_libraries \
@@ -9,13 +12,15 @@ docker run -d -h status.cryptofarmers.io -v prometheus:/prometheus -v /data/moni
   --web.external-url=http://status.cryptofarmers.io:9090
 
 #Run Grafana
-docker run -d -h status.cryptofarmers.io \
-  -v /data/monitoring/grafana/db:/var/lib/grafana -v /data/monitoring/grafana/grafana_admin_password:/run/secrets/grafana_admin_password \
-  -p 80:3000 --name grafana --restart on-failure:3 --security-opt="no-new-privileges=true" \
+docker run -d -p 80:3000 --name grafana --restart on-failure:3 --security-opt="no-new-privileges=true" \
+  -v /data/monitoring/grafana/db:/var/lib/grafana -v /data/monitoring/grafana/admin_password:/run/secrets/grafana_admin_password \
   -e GF_SERVER_DOMAIN=cryptofarmers.io \
   -e GF_SERVER_ROOT_URL=http://status.cryptofarmers.io \
   -e GF_SECURITY_ADMIN_PASSWORD__FILE=/run/secrets/grafana_admin_password \
   -e GF_USERS_ALLOW_ORG_CREATE=false \
   -e GF_SESSION_COOKIE_SECURE=true \
-  --health-cmd='curl -f localhost:3000/metrics || exit 1' \
+  -e GF_AUTH_ANONYMOUS_ENABLED=true \
+  -e GF_AUTH_ANONYMOUS_ORG_NAME="Crypto Farmers" \
+  -e GF_AUTH_ANONYMOUS_ORG_ROLE="Viewer" \
+  --network monitoring --health-cmd='curl -f localhost:3000/metrics || exit 1' \
   grafana/grafana:8.1.5
