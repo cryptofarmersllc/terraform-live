215175ab648d877e5227508d2a2867f44c5b9e9b
Shibu Gope <shibug@gmail.com>
2021-10-10 18:35:35 -0500

    changes

--- azure/scripts/configure.sh
+++ azure/scripts/configure.sh
@@ -1,12 +1,15 @@
 #Generate key pairs
 wget https://github.com/ethereum/eth2.0-deposit-cli/releases/download/v1.2.0/eth2deposit-cli-256ea21-linux-amd64.tar.gz
 tar -xvzf eth2deposit-cli-256ea21-linux-amd64.tar.gz
-cd eth2deposit-cli-256ea21-linux-amd64
+mv eth2deposit-cli-256ea21-linux-amd64 eth2deposit-cli
+cd eth2deposit-cli
+create secret.txt
 
 # Run your beacon node
-docker run -d -h validator1.cryptofarmers.io -v /data/ethereum:/data -p 4000:4000 -p 8080:8080 -p 13000:13000 -p 12000:12000/udp \
+docker run -d -h validator2.cryptofarmers.io -v /data/ethereum/beacon:/data -v /data/ethereum/logs:/logs \
+  -p 4000:4000 -p 8080:8080 -p 13000:13000 -p 12000:12000/udp \
   --name beacon-node --restart on-failure:3 --security-opt="no-new-privileges=true" \
-  gcr.io/prysmaticlabs/prysm/beacon-chain:stable \
+  gcr.io/prysmaticlabs/prysm/beacon-chain:v2.0.1 \
   --datadir=/data \
   --rpc-host=0.0.0.0 \
   --monitoring-host=0.0.0.0 \
@@ -16,22 +19,32 @@ docker run -d -h validator1.cryptofarmers.io -v /data/ethereum:/data -p 4000:400
   --accept-terms-of-use
 
 # Import your validator accounts into Prysm
-docker run -it -v $HOME/eth2-deposit-cli/validator_keys:/keys \
+docker run -it -v $HOME/eth2deposit-cli/validator_keys:/keys \
   -v /data/ethereum/wallet:/wallet \
   --name validator \
-  gcr.io/prysmaticlabs/prysm/validator:stable \
+  gcr.io/prysmaticlabs/prysm/validator:v2.0.1 \
   accounts import --keys-dir=/keys --wallet-dir=/wallet
 
   # Run your validator
-docker run -d -h validator1.cryptofarmers.io \
+docker run -d -h validator2.cryptofarmers.io \
   -v /data/ethereum/wallet:/wallet -v /data/ethereum/validatorDB:/validatorDB -v /data/ethereum/logs:/logs \
   --network="host" --restart on-failure:3 --security-opt="no-new-privileges=true" \
-  --name validator gcr.io/prysmaticlabs/prysm/validator:stable \
+  --name validator gcr.io/prysmaticlabs/prysm/validator:v2.0.1 \
   --beacon-rpc-provider=127.0.0.1:4000 \
   --monitoring-host=0.0.0.0 \
   --wallet-dir=/wallet \
   --wallet-password-file=/wallet/secret.txt \
   --datadir=/validatorDB \
   --log-file=/logs/validator.log \
-  --graffiti="Crypto Farmers Node 1: Happy to attest for Tony Paul Antony" \
+  --graffiti="Crypto Farmers Node 2: Happy to attest for Hariharasuthan Radhakrishnan" \
   --accept-terms-of-use
+
+#Rsync beacon node database
+#Copy private key to destination node
+scp id_rsa id_rsa.pub config validator2.cryptofarmers.io:/home/groot/.ssh/
+#verify ssh connection is established from destination to source node
+ssh use2lvalidator001prod -p 1122
+#Rsync beacon node database from source to destination node while beacon node is running
+rsync --progress -avzhe "ssh -p 1122" use2lvalidator001prod:/data/ethereum/beaconchaindata/beaconchain.db .
+#Rsync beacon node database from source to destination node while beacon node is stopped
+rsync --progress -avzhe "ssh -p 1122" use2lvalidator001prod:/data/ethereum/beaconchaindata/beaconchain.db .
