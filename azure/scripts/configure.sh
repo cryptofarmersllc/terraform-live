681ee4e21c18cb3438ed07a41d86895bcc4ca492
Shibu Gope <shibug@gmail.com>
2022-02-13 23:33:51 -0600

    changes

--- azure/scripts/configure.sh
+++ azure/scripts/configure.sh
@@ -19,7 +19,7 @@ rm -fr  ~/.ssh/id_rsa ~/.ssh/id_rsa.pub ~/.ssh/config
 docker run -d -v /data/ethereum/beacon:/data -v /data/ethereum/logs:/logs \
   -p 4000:4000 -p 8080:8080 -p 13000:13000 -p 12000:12000/udp \
   --name beacon-node --restart on-failure:3 --security-opt="no-new-privileges=true" \
-  gcr.io/prysmaticlabs/prysm/beacon-chain:v2.0.5 \
+  gcr.io/prysmaticlabs/prysm/beacon-chain:v2.0.6 \
   --datadir=/data \
   --rpc-host=0.0.0.0 \
   --monitoring-host=0.0.0.0 \
@@ -27,6 +27,18 @@ docker run -d -v /data/ethereum/beacon:/data -v /data/ethereum/logs:/logs \
   --fallback-web3provider=https://eth-mainnet.alchemyapi.io/v2/sCbtfxyKigCsKteX3M_pLT-KnboRLMcI \
   --log-file=/logs/beacon-node.log \
   --accept-terms-of-use
+
+docker run -d -v /data/ethereum/beacon:/data -v /data/ethereum/logs:/logs \
+  -p 4000:4000 -p 8080:8080 -p 13000:13000 -p 12000:12000/udp \
+  --name beacon-node --restart on-failure:3 --security-opt="no-new-privileges=true" \
+  gcr.io/prysmaticlabs/prysm/beacon-chain:v2.0.6 \
+  --datadir=/data \
+  --rpc-host=0.0.0.0 \
+  --monitoring-host=0.0.0.0 \
+  --http-web3provider=https://eth-mainnet.alchemyapi.io/v2/sCbtfxyKigCsKteX3M_pLT-KnboRLMcI \
+  --fallback-web3provider=https://mainnet.infura.io/v3/e688007f8726451192c518e37fe0cdda \
+  --log-file=/logs/beacon-node.log \
+  --accept-terms-of-use  
 
 #Generate key pairs
 wget https://github.com/ethereum/eth2.0-deposit-cli/releases/download/v1.2.0/eth2deposit-cli-256ea21-linux-amd64.tar.gz
@@ -40,20 +52,20 @@ create secret.txt
 docker run -it -v $HOME/eth2deposit-cli/validator_keys:/keys \
   -v /data/ethereum/wallet:/wallet \
   --name validator \
-  gcr.io/prysmaticlabs/prysm/validator:v2.0.5 \
+  gcr.io/prysmaticlabs/prysm/validator:v2.0.6 \
   accounts import --keys-dir=/keys --wallet-dir=/wallet
 
 #Run your validator
 docker run -d -v /data/ethereum/wallet:/wallet -v /data/ethereum/validatorDB:/validatorDB -v /data/ethereum/logs:/logs \
   --network="host" --restart on-failure:3 --security-opt="no-new-privileges=true" \
-  --name validator gcr.io/prysmaticlabs/prysm/validator:v2.0.5 \
+  --name validator gcr.io/prysmaticlabs/prysm/validator:v2.0.6 \
   --beacon-rpc-provider=127.0.0.1:4000 \
   --monitoring-host=0.0.0.0 \
   --wallet-dir=/wallet \
   --wallet-password-file=/wallet/secret.txt \
   --datadir=/validatorDB \
   --log-file=/logs/validator.log \
-  --graffiti="Crypto Farmers Node 5" \
+  --graffiti="Crypto Farmers Node 7" \
   --accept-terms-of-use
 
 #Setup notification on beaconchain for the new node
