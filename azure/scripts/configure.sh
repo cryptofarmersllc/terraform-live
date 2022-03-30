c3c5e42850fba10b0ccf9ce9b930377518ce551f
Shibu Gope <shibug@gmail.com>
2022-03-30 17:58:51 -0500

    added Node 8

--- azure/scripts/configure.sh
+++ azure/scripts/configure.sh
@@ -5,20 +5,21 @@ scp ~/.ssh/id_rsa ~/.ssh/id_rsa.pub ~/.ssh/config validator2.cryptofarmers.io:/h
 ssh use2lvalidator001prod -p 1122
 exit
 #Rsync beacon node database from source to destination node while source beacon node is running
-cd /data/ethereum/beacon
+cd /data/ethereum/node1/beacon
 mkdir beaconchaindata
 cd beaconchaindata
-rsync --progress -avzhe "ssh -p 1122" use2lvalidator002prod:/data/ethereum/beacon/beaconchaindata/beaconchain.db .
+rsync --progress -avzhe "ssh -p 1122" use2lvalidator002prod:/data/ethereum/node1/beacon/beaconchaindata/beaconchain.db .
 #Rsync beacon node database from source to destination node while source beacon node is stopped
-rsync --progress -avzhe "ssh -p 1122" use2lvalidator002prod:/data/ethereum/beacon/beaconchaindata/beaconchain.db .
+rsync --progress -avzhe "ssh -p 1122" use2lvalidator002prod:/data/ethereum/node1/beacon/beaconchaindata/beaconchain.db .
 #Remove private keys from destination node
 rm -fr  ~/.ssh/id_rsa ~/.ssh/id_rsa.pub ~/.ssh/config
 
 ------------------------------------------------------------
 #Run your beacon node
-docker run -d -v /data/ethereum/beacon:/data -v /data/ethereum/logs:/logs \
+#Node 1
+docker run -d -v /data/ethereum/node1/beacon:/data -v /data/ethereum/node1/logs:/logs \
   -p 4000:4000 -p 8080:8080 -p 13000:13000 -p 12000:12000/udp \
-  --name beacon-node --restart on-failure:3 --security-opt="no-new-privileges=true" \
+  --name beacon-node-1 --restart on-failure:3 --security-opt="no-new-privileges=true" \
   gcr.io/prysmaticlabs/prysm/beacon-chain:v2.0.6 \
   --datadir=/data \
   --rpc-host=0.0.0.0 \
@@ -28,9 +29,10 @@ docker run -d -v /data/ethereum/beacon:/data -v /data/ethereum/logs:/logs \
   --log-file=/logs/beacon-node.log \
   --accept-terms-of-use
 
-docker run -d -v /data/ethereum/beacon:/data -v /data/ethereum/logs:/logs \
-  -p 4000:4000 -p 8080:8080 -p 13000:13000 -p 12000:12000/udp \
-  --name beacon-node --restart on-failure:3 --security-opt="no-new-privileges=true" \
+#Node 2
+docker run -d -v /data/ethereum/node2/beacon:/data -v /data/ethereum/node2/logs:/logs \
+  -p 4002:4002 -p 8082:8082 -p 13002:13002 -p 12002:12002/udp \
+  --name beacon-node-2 --restart on-failure:3 --security-opt="no-new-privileges=true" \
   gcr.io/prysmaticlabs/prysm/beacon-chain:v2.0.6 \
   --datadir=/data \
   --rpc-host=0.0.0.0 \
@@ -38,34 +40,61 @@ docker run -d -v /data/ethereum/beacon:/data -v /data/ethereum/logs:/logs \
   --http-web3provider=https://eth-mainnet.alchemyapi.io/v2/sCbtfxyKigCsKteX3M_pLT-KnboRLMcI \
   --fallback-web3provider=https://mainnet.infura.io/v3/e688007f8726451192c518e37fe0cdda \
   --log-file=/logs/beacon-node.log \
-  --accept-terms-of-use  
+  --accept-terms-of-use \
+  --monitoring-port=8082 \
+  --p2p-tcp-port=13002 \
+  --p2p-udp-port=12002 \
+  --rpc-port=4002
 
 #Generate key pairs
-wget https://github.com/ethereum/eth2.0-deposit-cli/releases/download/v1.2.0/eth2deposit-cli-256ea21-linux-amd64.tar.gz
-tar -xvzf eth2deposit-cli-256ea21-linux-amd64.tar.gz
-mv eth2deposit-cli-256ea21-linux-amd64 eth2deposit-cli
-cd eth2deposit-cli
-./deposit existing-mnemonic --validator_start_index 2 --num_validators 2 --chain mainnet
+wget https://github.com/ethereum/staking-deposit-cli/releases/download/v2.1.0/staking_deposit-cli-ce8cbb6-linux-amd64.tar.gz
+tar -xvzf staking_deposit-cli-ce8cbb6-linux-amd64.tar.gz
+mv staking_deposit-cli-ce8cbb6-linux-amd64 staking_deposit-cli
+cd staking_deposit-cli
+./deposit existing-mnemonic --validator_start_index 7 --num_validators 1 --chain mainnet
 create secret.txt
 
 #Import your validator accounts into Prysm
-docker run -it -v $HOME/eth2deposit-cli/validator_keys:/keys \
-  -v /data/ethereum/wallet:/wallet \
-  --name validator \
+#Node 1
+docker run -it -v $HOME/staking_deposit-cli/validator_keys:/keys \
+  -v /data/ethereum/node1/wallet:/wallet \
+  --name validator-1 \
   gcr.io/prysmaticlabs/prysm/validator:v2.0.6 \
   accounts import --keys-dir=/keys --wallet-dir=/wallet
 
+#Node 2
+docker run -it -v $HOME/staking_deposit-cli/validator_keys:/keys \
+  -v /data/ethereum/node2/wallet:/wallet \
+  --name validator-2 \
+  gcr.io/prysmaticlabs/prysm/validator:v2.0.6 \
+  accounts import --keys-dir=/keys --wallet-dir=/wallet  
+
 #Run your validator
-docker run -d -v /data/ethereum/wallet:/wallet -v /data/ethereum/validatorDB:/validatorDB -v /data/ethereum/logs:/logs \
+#Node 1
+docker run -d -v /data/ethereum/node1/wallet:/wallet -v /data/ethereum/node1/validatorDB:/validatorDB -v /data/ethereum/node1/logs:/logs \
   --network="host" --restart on-failure:3 --security-opt="no-new-privileges=true" \
-  --name validator gcr.io/prysmaticlabs/prysm/validator:v2.0.6 \
+  --name validator-1 gcr.io/prysmaticlabs/prysm/validator:v2.0.6 \
   --beacon-rpc-provider=127.0.0.1:4000 \
   --monitoring-host=0.0.0.0 \
   --wallet-dir=/wallet \
   --wallet-password-file=/wallet/secret.txt \
   --datadir=/validatorDB \
   --log-file=/logs/validator.log \
-  --graffiti="Crypto Farmers Node 7" \
+  --graffiti="Crypto Farmers Node 4" \
+  --accept-terms-of-use
+
+#Node 2
+docker run -d -v /data/ethereum/node2/wallet:/wallet -v /data/ethereum/node2/validatorDB:/validatorDB -v /data/ethereum/node2/logs:/logs \
+  --network="host" --restart on-failure:3 --security-opt="no-new-privileges=true" \
+  --name validator-2 gcr.io/prysmaticlabs/prysm/validator:v2.0.6 \
+  --beacon-rpc-provider=127.0.0.1:4002 \
+  --monitoring-host=0.0.0.0 \
+  --monitoring-port=8083 \
+  --wallet-dir=/wallet \
+  --wallet-password-file=/wallet/secret.txt \
+  --datadir=/validatorDB \
+  --log-file=/logs/validator.log \
+  --graffiti="Crypto Farmers Node 8" \
   --accept-terms-of-use
 
 #Setup notification on beaconchain for the new node
