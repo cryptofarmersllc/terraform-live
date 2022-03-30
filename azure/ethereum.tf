2b9a45c4b2b9bb0d0eb761ee75f56d9e0c478919
Shibu Gope <shibug@gmail.com>
2022-03-30 18:46:10 -0500

    changes

--- azure/ethereum.tf
+++ azure/ethereum.tf
@@ -55,7 +55,7 @@ resource "azurerm_network_security_rule" "validatorInboundInternetAllow" {
   source_address_prefix       = "Internet"
   source_port_range           = "*"
   destination_address_prefix  = "VirtualNetwork"
-  destination_port_ranges     = ["1122", "4000", "4002", "13000", "13002"]
+  destination_port_ranges     = ["1122", "4000", "13000", "13002"]
   resource_group_name         = azurerm_resource_group.rg.name
   network_security_group_name = azurerm_network_security_group.validator.name
 }
