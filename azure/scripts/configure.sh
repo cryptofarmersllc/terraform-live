9b7b81ba6ec3f07b0de1c6c30a3783f4bb5b97c7
Shibu Gope <shibug@gmail.com>
2022-06-01 11:57:17 -0500

    changes

--- azure/scripts/configure.sh
+++ azure/scripts/configure.sh
@@ -20,20 +20,21 @@ rm -fr  ~/.ssh/id_rsa ~/.ssh/id_rsa.pub ~/.ssh/config
 docker run -d -v /data/ethereum/node1/beacon:/data -v /data/ethereum/node1/logs:/logs \
   -p 4000:4000 -p 8080:8080 -p 13000:13000 -p 12000:12000/udp \
   --name beacon-node-1 --restart on-failure:3 --security-opt="no-new-privileges=true" \
-  gcr.io/prysmaticlabs/prysm/beacon-chain:v2.0.6 \
+  gcr.io/prysmaticlabs/prysm/beacon-chain:v2.1.2 \
   --datadir=/data \
   --rpc-host=0.0.0.0 \
   --monitoring-host=0.0.0.0 \
   --http-web3provider=https://mainnet.infura.io/v3/e688007f8726451192c518e37fe0cdda \
   --fallback-web3provider=https://eth-mainnet.alchemyapi.io/v2/sCbtfxyKigCsKteX3M_pLT-KnboRLMcI \
   --log-file=/logs/beacon-node.log \
-  --accept-terms-of-use
+  --accept-terms-of-use \
+  --suggested-fee-recipient=0xa63Ce14Bc241812e3081A74b0b999b0D2bF0657F
 
 #Node 2
 docker run -d -v /data/ethereum/node2/beacon:/data -v /data/ethereum/node2/logs:/logs \
   -p 4002:4002 -p 8082:8082 -p 13002:13002 -p 12002:12002/udp \
   --name beacon-node-2 --restart on-failure:3 --security-opt="no-new-privileges=true" \
-  gcr.io/prysmaticlabs/prysm/beacon-chain:v2.0.6 \
+  gcr.io/prysmaticlabs/prysm/beacon-chain:v2.1.2 \
   --datadir=/data \
   --rpc-host=0.0.0.0 \
   --monitoring-host=0.0.0.0 \
@@ -44,7 +45,8 @@ docker run -d -v /data/ethereum/node2/beacon:/data -v /data/ethereum/node2/logs:
   --monitoring-port=8082 \
   --p2p-tcp-port=13002 \
   --p2p-udp-port=12002 \
-  --rpc-port=4002
+  --rpc-port=4002 \
+  --suggested-fee-recipient=0xa63Ce14Bc241812e3081A74b0b999b0D2bF0657F
 
 #Generate key pairs
 wget https://github.com/ethereum/staking-deposit-cli/releases/download/v2.1.0/staking_deposit-cli-ce8cbb6-linux-amd64.tar.gz
@@ -59,34 +61,35 @@ create secret.txt
 docker run -it -v $HOME/staking_deposit-cli/validator_keys:/keys \
   -v /data/ethereum/node1/wallet:/wallet \
   --name validator-1 \
-  gcr.io/prysmaticlabs/prysm/validator:v2.0.6 \
+  gcr.io/prysmaticlabs/prysm/validator:v2.1.2 \
   accounts import --keys-dir=/keys --wallet-dir=/wallet
 
 #Node 2
 docker run -it -v $HOME/staking_deposit-cli/validator_keys:/keys \
   -v /data/ethereum/node2/wallet:/wallet \
   --name validator-2 \
-  gcr.io/prysmaticlabs/prysm/validator:v2.0.6 \
+  gcr.io/prysmaticlabs/prysm/validator:v2.1.2 \
   accounts import --keys-dir=/keys --wallet-dir=/wallet  
 
 #Run your validator
 #Node 1
 docker run -d -v /data/ethereum/node1/wallet:/wallet -v /data/ethereum/node1/validatorDB:/validatorDB -v /data/ethereum/node1/logs:/logs \
   --network="host" --restart on-failure:3 --security-opt="no-new-privileges=true" \
-  --name validator-1 gcr.io/prysmaticlabs/prysm/validator:v2.0.6 \
+  --name validator-1 gcr.io/prysmaticlabs/prysm/validator:v2.1.2 \
   --beacon-rpc-provider=127.0.0.1:4000 \
   --monitoring-host=0.0.0.0 \
   --wallet-dir=/wallet \
   --wallet-password-file=/wallet/secret.txt \
   --datadir=/validatorDB \
   --log-file=/logs/validator.log \
-  --graffiti="Crypto Farmers Node 1" \
-  --accept-terms-of-use
+  --graffiti="Crypto Farmers Node 4" \
+  --accept-terms-of-use \
+  --suggested-fee-recipient=0xa63Ce14Bc241812e3081A74b0b999b0D2bF0657F
 
 #Node 2
 docker run -d -v /data/ethereum/node2/wallet:/wallet -v /data/ethereum/node2/validatorDB:/validatorDB -v /data/ethereum/node2/logs:/logs \
   --network="host" --restart on-failure:3 --security-opt="no-new-privileges=true" \
-  --name validator-2 gcr.io/prysmaticlabs/prysm/validator:v2.0.6 \
+  --name validator-2 gcr.io/prysmaticlabs/prysm/validator:v2.1.2 \
   --beacon-rpc-provider=127.0.0.1:4002 \
   --monitoring-host=0.0.0.0 \
   --monitoring-port=8083 \
@@ -94,8 +97,9 @@ docker run -d -v /data/ethereum/node2/wallet:/wallet -v /data/ethereum/node2/val
   --wallet-password-file=/wallet/secret.txt \
   --datadir=/validatorDB \
   --log-file=/logs/validator.log \
-  --graffiti="Crypto Farmers Node 5" \
-  --accept-terms-of-use
+  --graffiti="Crypto Farmers Node 8" \
+  --accept-terms-of-use \
+  --suggested-fee-recipient=0xa63Ce14Bc241812e3081A74b0b999b0D2bF0657F
 
 #Setup notification on beaconchain for the new node
 
