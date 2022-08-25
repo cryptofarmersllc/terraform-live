17cf84e62106279f084fd751be5e0f52cb87ffb7
Shibu Gope <shibug@gmail.com>
2022-08-25 09:18:06 -0500

    changes

--- azure/scripts/configure.sh
+++ azure/scripts/configure.sh
@@ -20,7 +20,7 @@ rm -fr  ~/.ssh/id_rsa ~/.ssh/id_rsa.pub ~/.ssh/config
 docker run -d -v /data/ethereum/node1/beacon:/data -v /data/ethereum/node1/logs:/logs \
   -p 4000:4000 -p 8080:8080 -p 13000:13000 -p 12000:12000/udp \
   --name beacon-node-1 --restart on-failure:3 --security-opt="no-new-privileges=true" \
-  gcr.io/prysmaticlabs/prysm/beacon-chain:v2.1.4 \
+  gcr.io/prysmaticlabs/prysm/beacon-chain:v3.0.0 \
   --datadir=/data \
   --rpc-host=0.0.0.0 \
   --monitoring-host=0.0.0.0 \
@@ -28,13 +28,14 @@ docker run -d -v /data/ethereum/node1/beacon:/data -v /data/ethereum/node1/logs:
   --fallback-web3provider=https://eth-mainnet.alchemyapi.io/v2/sCbtfxyKigCsKteX3M_pLT-KnboRLMcI \
   --log-file=/logs/beacon-node.log \
   --accept-terms-of-use \
+  --p2p-max-peers 23 \
   --suggested-fee-recipient=0xa63Ce14Bc241812e3081A74b0b999b0D2bF0657F
 
 #Node 2
 docker run -d -v /data/ethereum/node2/beacon:/data -v /data/ethereum/node2/logs:/logs \
   -p 4002:4002 -p 8082:8082 -p 13002:13002 -p 12002:12002/udp \
   --name beacon-node-2 --restart on-failure:3 --security-opt="no-new-privileges=true" \
-  gcr.io/prysmaticlabs/prysm/beacon-chain:v2.1.4 \
+  gcr.io/prysmaticlabs/prysm/beacon-chain:v3.0.0 \
   --datadir=/data \
   --rpc-host=0.0.0.0 \
   --monitoring-host=0.0.0.0 \
@@ -46,6 +47,7 @@ docker run -d -v /data/ethereum/node2/beacon:/data -v /data/ethereum/node2/logs:
   --p2p-tcp-port=13002 \
   --p2p-udp-port=12002 \
   --rpc-port=4002 \
+  --p2p-max-peers 23 \
   --suggested-fee-recipient=0xa63Ce14Bc241812e3081A74b0b999b0D2bF0657F
 
 #Generate key pairs
@@ -61,21 +63,21 @@ create secret.txt
 docker run -it -v $HOME/staking_deposit-cli/validator_keys:/keys \
   -v /data/ethereum/node1/wallet:/wallet \
   --name validator-1 \
-  gcr.io/prysmaticlabs/prysm/validator:v2.1.4 \
+  gcr.io/prysmaticlabs/prysm/validator:v3.0.0 \
   accounts import --keys-dir=/keys --wallet-dir=/wallet
 
 #Node 2
 docker run -it -v $HOME/staking_deposit-cli/validator_keys:/keys \
   -v /data/ethereum/node2/wallet:/wallet \
   --name validator-2 \
-  gcr.io/prysmaticlabs/prysm/validator:v2.1.4 \
+  gcr.io/prysmaticlabs/prysm/validator:v3.0.0 \
   accounts import --keys-dir=/keys --wallet-dir=/wallet  
 
 #Run your validator
 #Node 1
 docker run -d -v /data/ethereum/node1/wallet:/wallet -v /data/ethereum/node1/validatorDB:/validatorDB -v /data/ethereum/node1/logs:/logs \
   --network="host" --restart on-failure:3 --security-opt="no-new-privileges=true" \
-  --name validator-1 gcr.io/prysmaticlabs/prysm/validator:v2.1.4 \
+  --name validator-1 gcr.io/prysmaticlabs/prysm/validator:v3.0.0 \
   --beacon-rpc-provider=127.0.0.1:4000 \
   --monitoring-host=0.0.0.0 \
   --wallet-dir=/wallet \
@@ -89,7 +91,7 @@ docker run -d -v /data/ethereum/node1/wallet:/wallet -v /data/ethereum/node1/val
 #Node 2
 docker run -d -v /data/ethereum/node2/wallet:/wallet -v /data/ethereum/node2/validatorDB:/validatorDB -v /data/ethereum/node2/logs:/logs \
   --network="host" --restart on-failure:3 --security-opt="no-new-privileges=true" \
-  --name validator-2 gcr.io/prysmaticlabs/prysm/validator:v2.1.4 \
+  --name validator-2 gcr.io/prysmaticlabs/prysm/validator:v3.0.0 \
   --beacon-rpc-provider=127.0.0.1:4002 \
   --monitoring-host=0.0.0.0 \
   --monitoring-port=8083 \
--- azure/scripts/configure_executor.sh
+++ azure/scripts/configure_executor.sh
@@ -3,11 +3,11 @@
 ------------------------------------------------------------
 #Run your executor node
 #Node 1
-docker run -d --name ethereum-node \
+docker run -d --name executor-node \
   -v /data/ethereum/data:/root/.ethereum -v /data/ethereum/config:/root/config \
-  -p 8545:8545 -p 30303:30303 \
+  -p 6060:6060 -p 8551:8551 -p 30303:30303/tcp -p 30303:30303/udp \
   --restart on-failure:3 --security-opt="no-new-privileges=true" \
-  ethereum/client-go:v1.10.22 \
+  ethereum/client-go:v1.10.23 \
   --mainnet \
   --syncmode full \
   --authrpc.addr 0.0.0.0 \
