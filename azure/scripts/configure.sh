3f63fc1c6b4b00a3e9ae2f1981931fd01d02659d
Justin <drakefjustin@gmail.com>
2022-12-31 17:09:14 +0100

    add ultra sound relay
    add relay.ultrasound.money

--- azure/scripts/configure.sh
+++ azure/scripts/configure.sh
@@ -99,7 +99,7 @@ docker run -d -v /data/ethereum/wallet:/wallet -v /data/ethereum/validatorDB:/va
 docker run -d --network="host" --restart on-failure:3 --security-opt="no-new-privileges=true" \
 --name mev-boost flashbots/mev-boost:v1.4.0 \
 -mainnet \
--relay-check -relays https://0xac6e77dfe25ecd6110b8e780608cce0dab71fdd5ebea22a16c0205200f2f8e2e3ad3b71d3499c54ad14d6c21b41a37ae@boost-relay.flashbots.net
+-relay-check -relays https://0xac6e77dfe25ecd6110b8e780608cce0dab71fdd5ebea22a16c0205200f2f8e2e3ad3b71d3499c54ad14d6c21b41a37ae@boost-relay.flashbots.net,https://0xa1559ace749633b997cb3fdacffb890aeebdb0f5a3b6aaa7eeeaf1a38af0a8fe88b9e4b1f61f236d2e64d95733327a62@relay.ultrasound.money
 #Setup notification on beaconchain for the new node
 
 --------------------------------------------------------------------------------------------------------------------------
