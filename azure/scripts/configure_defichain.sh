77209afb0f9dcfd4d24bdd43fe64e0a573b14149
Shibu Gope <shibug@gmail.com>
2023-02-05 22:06:38 -0600

    upgrade

--- azure/scripts/configure_defichain.sh
+++ azure/scripts/configure_defichain.sh
@@ -1,7 +1,7 @@
 docker run -d --name master-node \
 -v /data/defichain:/home/defichain/data -p 8555:8555 \
 --restart on-failure:3 --security-opt="no-new-privileges=true" \
-shibug/defichain:3.2.2
+shibug/defichain:3.2.3
 
 #Get Block count
 dke master-node /home/defichain/.defi/defi-cli -datadir=/home/defichain/data getblockcount
