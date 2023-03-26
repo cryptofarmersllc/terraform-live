44965b85a6ba6d101da96c0509b70e70e9360174
Shibu Gope <shibug@gmail.com>
2023-03-26 15:38:12 -0500

    upgrade

--- azure/scripts/configure.sh
+++ azure/scripts/configure.sh
@@ -4,7 +4,7 @@ docker run -d --name executor-node \
   -v /data/ethereum/execution:/root/.ethereum -v /data/ethereum/config:/root/config \
   -p 6060:6060 -p 8551:8551 -p 30303:30303/tcp -p 30303:30303/udp \
   --restart on-failure:3 --security-opt="no-new-privileges=true" \
-  ethereum/client-go:v1.11.2 \
+  ethereum/client-go:v1.11.4 \
   --mainnet \
   --authrpc.addr 0.0.0.0 \
   --authrpc.jwtsecret=/root/config/jwt.hex \
@@ -19,14 +19,14 @@ docker run -d --name executor-node \
 #Prune Geth
 docker run -d --name prune-geth \
   -v /data/ethereum/data:/root/.ethereum \
-  ethereum/client-go:v1.11.2 \
+  ethereum/client-go:v1.11.4 \
   snapshot prune-state \
   --mainnet
 #Geth attach
-dki -v /data/ethereum/execution:/root/.ethereum ethereum/client-go:v1.11.2 attach
+dki -v /data/ethereum/execution:/root/.ethereum ethereum/client-go:v1.11.4 attach
 
 #Geth removedb
-dki -v /data/ethereum/execution:/root/.ethereum -v /data/ethereum/config:/root/config ethereum/client-go:v1.11.2 removedb
+dki -v /data/ethereum/execution:/root/.ethereum -v /data/ethereum/config:/root/config ethereum/client-go:v1.11.4 removedb
 Yes to remove db, no to remove ancient db
 
 #How to rewind blockchain head
@@ -38,7 +38,7 @@ Yes to remove db, no to remove ancient db
 #Run your beacon node
 docker run -d -v /data/ethereum/beacon:/data -v /data/ethereum/logs:/logs -v /data/ethereum/config:/config \
   --network="host" --name beacon-node-1 --restart on-failure:3 --security-opt="no-new-privileges=true" \
-  gcr.io/prysmaticlabs/prysm/beacon-chain:v3.2.1 \
+  gcr.io/prysmaticlabs/prysm/beacon-chain:v3.2.2 \
   --datadir=/data \
   --rpc-host=0.0.0.0 \
   --monitoring-host=0.0.0.0 \
@@ -61,7 +61,7 @@ create secret.txt
 #Node 1
 docker run -it --rm \
   -v $HOME/staking_deposit-cli/validator_keys:/keys -v /data/ethereum/node1/wallet:/wallet \
-  gcr.io/prysmaticlabs/prysm/validator:v3.2.1 \
+  gcr.io/prysmaticlabs/prysm/validator:v3.2.2 \
   accounts import --accept-terms-of-use \
   --keys-dir=/keys --account-password-file=/wallet/secret.txt \
   --wallet-dir=/wallet --wallet-password-file=/wallet/secret.txt
@@ -70,7 +70,7 @@ docker run -it --rm \
 #List accounts
 docker run -it --rm \
   -v /data/ethereum/node1/wallet:/wallet \
-  gcr.io/prysmaticlabs/prysm/validator:v3.2.1 \
+  gcr.io/prysmaticlabs/prysm/validator:v3.2.2 \
   accounts list --accept-terms-of-use --show-private-keys \
   --wallet-dir=/wallet --wallet-password-file=/wallet/secret.txt
   
@@ -78,7 +78,7 @@ docker run -it --rm \
 #List validator indices
 docker run -it --rm --network="host" \
   -v /data/ethereum/node1/wallet:/wallet \
-  gcr.io/prysmaticlabs/prysm/validator:v3.2.1 \
+  gcr.io/prysmaticlabs/prysm/validator:v3.2.2 \
   accounts list --accept-terms-of-use \
   --wallet-dir=/wallet --wallet-password-file=/wallet/secret.txt \
   --list-validator-indices --beacon-rpc-provider=127.0.0.1:4000
@@ -86,7 +86,7 @@ docker run -it --rm --network="host" \
 #Run your validator
 docker run -d -v /data/ethereum/wallet:/wallet -v /data/ethereum/validatorDB:/validatorDB -v /data/ethereum/logs:/logs \
   --network="host" --restart on-failure:3 --security-opt="no-new-privileges=true" \
-  --name validator-1 gcr.io/prysmaticlabs/prysm/validator:v3.2.1 \
+  --name validator-1 gcr.io/prysmaticlabs/prysm/validator:v3.2.2 \
   --beacon-rpc-provider=localhost:4000 \
   --monitoring-host=0.0.0.0 \
   --wallet-dir=/wallet \
@@ -95,11 +95,12 @@ docker run -d -v /data/ethereum/wallet:/wallet -v /data/ethereum/validatorDB:/va
   --log-file=/logs/validator.log \
   --graffiti="Crypto Farmers" \
   --accept-terms-of-use \
+  --enable-builder=true \
   --suggested-fee-recipient=0xa63Ce14Bc241812e3081A74b0b999b0D2bF0657F
 
 #Run mev-boost per node
 docker run -d --network="host" --restart on-failure:3 --security-opt="no-new-privileges=true" \
---name mev-boost flashbots/mev-boost:v1.4.0 \
+--name mev-boost flashbots/mev-boost:1.5.0 \
 -mainnet \
 -relay-check -relays https://0xac6e77dfe25ecd6110b8e780608cce0dab71fdd5ebea22a16c0205200f2f8e2e3ad3b71d3499c54ad14d6c21b41a37ae@boost-relay.flashbots.net,https://0xa1559ace749633b997cb3fdacffb890aeebdb0f5a3b6aaa7eeeaf1a38af0a8fe88b9e4b1f61f236d2e64d95733327a62@relay.ultrasound.money
 #Setup notification on beaconchain for the new node
